f918a8da659678f710c56283bfd5dce21ac4ffc0
hardik101 <hardikbalar101@gmail.com>
2014-04-17 21:10:55 +0200

    "content review changes"

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,36 +1,5 @@
 class StagesController < ApplicationController
 
 
-	def index 
-
-		@stages = Stage.all
-
-		respond_to do |format|
-        format.html # index.html.erb
-        format.json { render json: @stages }
-        end
-
-	end 
-
-	def create 
-
-	end 
-
-	def update 
-
-
-
-	end
-
-	def show 
-
-
-	end 
-
-	def destroy 
-
-
-	end 
-
 
 end
