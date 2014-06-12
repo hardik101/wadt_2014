677732ea5380346baa9164af6dee7e9585f8f185
hardik101 <hardikbalar101@gmail.com>
2014-06-12 11:37:24 +0200

    form changes

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,12 +3,26 @@ module DeviseHelper
     return '' if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    html = 
-    content_tag(:div, :class => 'alert alert-error alert-block') do
-        concat content_tag(:button,x,:class => 'close', :"data-dismiss" => 'alert')
-        concat content_tag(:p, messages)
-    end
-    
+   
+     html = <<-HTML
+    <div class="alert alert-error alert-block"> <button type="button"
+    class="close" data-dismiss="alert">x</button>
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
   end
 end
 
+
+/%
+
+ html = 
+    content_tag(:div, :class => 'alert alert-error alert-block') do
+        concat content_tag(:button,'x',:class => 'close', :"data-dismiss" => 'alert')
+        concat content_tag(:li, messages)
+    end 
+
+
+    %/
