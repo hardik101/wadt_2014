02c802503d65f48eb30b61cd373b8d3046c39640
hardik101 <hardikbalar101@gmail.com>
2014-06-26 15:18:50 +0200

    WADT_ref

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -3,12 +3,12 @@ class MemberMailer < ActionMailer::Base
 
 	def welcome_email(member)
 		@member =member
-		mail(to: @member.email, subject: "Thank you for registering for the WADT-2014")
+		mail(to: @member.email, subject: "Thank you for registering for the #{Settings.name}")
 	end
 
 	def new_user_registered_email(member)
 		@member = member
-		mail(to: Settings.contact ,subject:"New user registered for WADT")
+		mail(to: Settings.contact ,subject:"New member registered for #{Settings.name}")
 	end 
 
 end
