6ecfad94035046e612e9512793f219bacc0f7e55
hardik101 <hardikbalar101@gmail.com>
2014-07-21 15:33:38 +0200

    hotfix_admin

--- app/models/member.rb
+++ app/models/member.rb
@@ -9,7 +9,7 @@ class Member < ActiveRecord::Base
   REG_FOR =  ["WADT only","IFIP only","WADT and IFIP"]
   COUNTRY = ["Afghanistan","Albania","Algeria","Andorra","Angola","Antigua & Deps","Argentina","Armenia","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bhutan","Bolivia","Bosnia Herzegovina","Botswana","Brazil","Brunei","Bulgaria","Burkina","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Central African Rep","Chad","Chile","China","Colombia","Comoros","Congo","Congo {Democratic Rep}","Costa Rica","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Fiji","Finland","France","Gabon","Gambia","Georgia","Germany","Ghana","Greece","Grenada","Guatemala","Guinea","Guinea-Bissau","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland {Republic}","Israel","Italy","Ivory Coast","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Korea North","Korea South","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Morocco","Mozambique","Myanmar, {Burma}","Namibia","Nauru","Nepal","Netherlands","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Romania","Russian Federation","Rwanda","St Kitts & Nevis","St Lucia","Saint Vincent & the Grenadines","Samoa","San Marino","Sao Tome & Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Sudan","Spain","Sri Lanka","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe"]
   HIC_COUNTRY=["Germany","France"]
-  after_create :send_email_to_organizer 
+  after_create :send_email_to_organizer
 
   after_save :send_welcome_email, :if => proc { |l| l.confirmed_at_changed? && l.confirmed_at_was.nil? }
   
@@ -28,34 +28,36 @@ class Member < ActiveRecord::Base
   #  MemberMailer.new_user_registered_email(self).deliver
   
   #end
+# public
 
-  public
-    def is_country_hic
-      HIC_COUNTRY.each do |a|
-        if a == self.country
-          return true
-        else 
-          return false
-        end 
-       end 
-    end
+# def self.is_country_hic
+      #HIC_COUNTRY.each do |a|
+        #if a == current_member.country
+         # return true
+        #else 
+       #   return false
+      #  end 
+     #  end 
+    #end
 
-    def self.compute_fees
+    #def compute_fees
 
-    @fees=240;
-    if self.registered_for=="WADT only" && self.is_country_hic == true
-        @fees=350;
-    elsif self.registered_for=="WADT only" && self.is_country_hic == false
-        @fees = 230;
-    elsif self.registered_for=="WADT and IFIP" && self.is_country_hic == true
-        @fees =590;
-    elsif  self.registered_for=="WADT and IFIP" && self.is_country_hic == false
-        @fees =470;
-    else
-       return @fees
-     end 
-   end
-                  
+    #@fees=240;
+  #  if Member.registered_for=="WADT only" && Member.is_country_hic == true
+   #     @fees=350;
+    #elsif Member.registered_for=="WADT only" && Member.is_country_hic == false
+     #   @fees = 230;
+  # elsif Member.registered_for=="WADT and IFIP" && Member.is_country_hic == true
+   #     @fees =590;
+    #elsif  Member.registered_for=="WADT and IFIP" && Member.is_country_hic == false
+     #   @fees =470;
+    #else
+     #  return @fees
+     #end 
+   #end
+
+   
+               
 
   private
 
